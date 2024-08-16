fa6e154c97d1147668294610278ee0b7588cb3b7
Key <keynarafaelp@gmail.com>
2024-08-16 17:45:41 -0500

    fix filtering account type transactions

--- tap_unanet/streams.py
+++ tap_unanet/streams.py
@@ -227,7 +227,7 @@ class PnLDetailStream(UnanetStream):
                 "gl_key","feature","post_date","fiscal_month_key","account_key","organization_key","document_number","reference","description","transaction_date","quantity","debit_amount","credit_amount","project_key","person_key","customer_key","local_debit_amount","local_credit_amount","instance_debit_amount","instance_credit_amount","transaction_currency","local_currency","account_code","account_key","account_type","account_name","organization_code","customer_code","organization_name","customer_name","organization_type_key","customer_type_key","organization_type","customer_type","person_code","person_first_name","person_last_name","project_name"
             ]
             combined_dict = dict(zip(properties_list, row))
-            if row.get("account_type") not in ["R", "E"]:
+            if combined_dict.get("account_type") not in ["R", "E"]:
                 return {}
             # Calculate net amount
             self.logger.info("Calculating totals for net amount...")
